bedc6f73abe5f3eefb6566ae4374748aa3081631
hduhaime <hduhaime@umich.edu>
2015-01-17 05:36:48 -0500

    first commit

---
+++ Assets/Myo/Scripts/changecolor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class changecolor : MonoBehaviour {
+
+	public GameObject myo = null;
+
+	private Pose _lastPose = Pose.Unknown;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+
+		if (thalmicMyo.pose != _lastPose) {
+
+						_lastPose = thalmicMyo.pose;
+						if (thalmicMyo.pose == Pose.Fist) {
+								thalmicMyo.Vibrate (VibrationType.Medium);
+								// Change material when wave in, wave out or double tap poses are made.
+						} 
+				}
+		print (thalmicMyo.accelerometer);
+		print ("hello");
+
+	}
+}
